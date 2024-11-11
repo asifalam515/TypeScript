a7a43ca9607967ca584235789d86c974d8d0897e
asifalam515 <asibulalam515@gmail.com>
2024-11-11 16:53:12 +0600

    started

---
+++ first_typeScript/variable.js
@@ -0,0 +1,6 @@
+var myMovie = "Forrest Gump";
+var myNumber = 225;
+var gameOver = true;
+gameOver = false;
+// gameOver = "fasle";
+console.log(myNumber);
---
+++ first_typeScript/variable.ts
@@ -0,0 +1,14 @@
+const myMovie: string = "Forrest Gump";
+let myNumber: number = 225;
+let gameOver: boolean = true;
+gameOver = false;
+// gameOver = "fasle";
+console.log(myNumber);
+// type inference :typesCript will automatically detect the data type
+let tvShow = "friends";
+
+// Any:it turns off type checking.doesn't care about what is the type is
+let x: any = 243;
+
+x = "how are you";
+x = true;
